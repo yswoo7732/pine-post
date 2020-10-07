167ef5b24c0f1006aa0273e730f9401b0fc27544
우윤선(Woo Yun Seon) <yunseon.woo@hanwha.com>
2020-10-07 18:04:30 +0900

    12/13 작업중

--- src/0A0013/index.js
+++ src/0A0013/index.js
@@ -3,27 +3,27 @@ window.onload = function () {
     document.getElementsByClassName("container")[0].classList.add("border_none");
   }
 
-  // var swiper = new Swiper(".swiper-container", {
-  //   direction: "horizontal",
-  //   initialSlide: 0,
-  //   on: {
-  //     slideChange: function (sp) {
-  //       switch (sp.activeIndex) {
-  //         case 1:
-  //           break;
-  //         case 2:
-  //           break;
-  //         case 3:
-  //           break;
-  //         default:
-  //           console.log("switch default activeIndex:", sp.activeIndex);
-  //       }
-  //     },
-  //   },
-  //   pagination: {
-  //     el: ".swiper-pagination",
-  //   },
-  // });
+  var swiper = new Swiper(".swiper-container", {
+    direction: "horizontal",
+    initialSlide: 0,
+    on: {
+      slideChange: function (sp) {
+        switch (sp.activeIndex) {
+          case 1:
+            break;
+          case 2:
+            break;
+          case 3:
+            break;
+          default:
+            console.log("switch default activeIndex:", sp.activeIndex);
+        }
+      },
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+  });
   
   var bContents = document.getElementsByClassName("b_contents")[0];
   var basicFoot = document.getElementsByClassName("basic_foot")[0];
