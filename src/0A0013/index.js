f995136a65970ef43794dde8a6bfaeb87df52c46
우윤선(Woo Yun Seon) <yunseon.woo@hanwha.com>
2020-10-08 17:48:39 +0900

    12, 13 modified

--- src/0A0013/index.js
+++ src/0A0013/index.js
@@ -2,28 +2,6 @@ window.onload = function () {
   if (/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent)) {
     document.getElementsByClassName("container")[0].classList.add("border_none");
   }
-
-  var swiper = new Swiper(".swiper-container", {
-    direction: "horizontal",
-    initialSlide: 0,
-    on: {
-      slideChange: function (sp) {
-        switch (sp.activeIndex) {
-          case 1:
-            break;
-          case 2:
-            break;
-          case 3:
-            break;
-          default:
-            console.log("switch default activeIndex:", sp.activeIndex);
-        }
-      },
-    },
-    pagination: {
-      el: ".swiper-pagination",
-    },
-  });
   
   var bContents = document.getElementsByClassName("b_contents")[0];
   var basicFoot = document.getElementsByClassName("basic_foot")[0];
