077fd796e77043a42f3553adb18ab9dc93b97e14
우윤선(Woo Yun Seon) <yunseon.woo@hanwha.com>
2020-11-03 13:32:55 +0900

    기본형 1차 작업

--- src/0A0005/index.js
+++ src/0A0005/index.js
@@ -10,101 +10,72 @@ window.onload = function () {
   var bContents = document.getElementsByClassName("b_contents")[0];
   var basicFoot = document.getElementsByClassName("basic_foot")[0];
   var boxShadows = document.getElementsByClassName("box-shadow");
-  var height = window.screen.height / 2;
   var prevScrollTop = 0;
-  var wrap_body = document.getElementById("body_frame");
-  var cover_flag = false;
+  var height = window.screen.height / 2;
+  var wrapBody = document.getElementById("body_frame");
+  var coverImg = document.getElementById("cover_full");
+  var linkImg = document.getElementById("link_cover_full");
+  var body = document.body;
+
+  var flag = false;
+  var state = 0;
 
   document.addEventListener("scroll", function (event) {
-    
-    // scroll 맨마지막일때 - 하단 열기 준비
-    if (Math.round(window.scrollY) >= document.body.scrollHeight + document.getElementById("link_cover_full").scrollHeight && !cover_flag) {
-      console.log("하단 열기 준비: ", cover_flag)
+    var basicFootBottom = Math.round(basicFoot.getBoundingClientRect().bottom);
+    var currentPercentage =
+      (window.scrollY / (body.scrollHeight - window.screen.height - basicFoot.scrollHeight - linkImg.scrollHeight)) * 100;
 
-      // cover_flag = false;
+    console.log("state: ", state);
+    switch (state) {
+      case 0: // 하단 비활성
+        Array.from(boxShadows).forEach(function (element, index, array) {
+          if (!element.classList.contains("box-shadow-active") && element.offsetTop < document.documentElement.scrollTop + height) {
+            element.classList.add("box-shadow-active");
+          }
+        });
 
-      // if (!cover_flag) {
-      //   console.log("하단 연 상태")
-        basicFoot.style.display = "none";
+        // scroll progress bar
+        document.getElementsByClassName("progress-bar")[0].style.width = currentPercentage + "%";
 
-        document.getElementsByClassName("wrap_body_frame")[0].style.position = "fixed";
-        document.getElementsByClassName("wrap_body_frame")[0].style.bottom = 0;
-        
-        document.getElementById("link_cover_full").style.display = "block";
-        document.getElementById("link_cover_full").style.position = "absolute";
-        document.getElementById("link_cover_full").style.zIndex = 60;
-        cover_flag = true;
-
-      // }
-    } 
-    if(document.getElementById("link_cover_full").getBoundingClientRect().top < window.innerHeight && cover_flag) {
-      // 하단 연 상태
-        console.log("하단 연 상태: ", cover_flag)
-        document.getElementById("link_cover_full").style.top = window.innerHeight - document.documentElement.scrollTop + "px";
-        document.getElementById("link_cover_full").style.height = 100 + "vh";
-
-        // prevScrollTop = window.scrollY;
-    }
-    if (document.documentElement.scrollTop == 0 && document.getElementById("link_cover_full").getBoundingClientRect().top >= window.innerHeight && cover_flag) {
-    // 하단 이미지가 내려갔을때
-     
-      console.log("하단 이미지 내려갔을때: ", cover_flag);
-      
-      // basicFoot.style.display = "";
-
-      // document.getElementsByClassName("wrap_body_frame")[0].style.position = "";
-      // document.getElementsByClassName("wrap_body_frame")[0].style.bottom = "";
-
-      // document.getElementById("link_cover_full").style.position = "absolute";
-      // document.getElementById("link_cover_full").style.top = "";
-      // // document.getElementById("link_cover_full").style.zIndex = 0;
-      // document.getElementById("link_cover_full").style.height = 5 + "vh";
-
-      // window.scrollTo(0, prevScrollTop);
-    //  cover_flag = false;
-     
-    } 
-    else {
-      console.log("메인상태: ", cover_flag)
-      console.log("메인상태: ", document.getElementById("link_cover_full").getBoundingClientRect().top, window.innerHeight - document.documentElement.scrollTop)
-      // box shadow
-      Array.from(boxShadows).forEach(function (element, index, array) {
-        // console.log("box shadow: ",element.offsetTop + element.offsetWidth)
-        // console.log("window.scrollY: ",window.scrollY)
-        if (
-          !element.classList.contains("box-shadow-active") &&
-          element.offsetTop + element.offsetWidth < window.scrollY
-        ) {
-          element.classList.add("box-shadow-active");
+        // 좋아요 공유버튼 이벤트 sticky
+        if (document.documentElement.scrollTop == 0 || Math.round(window.scrollY) == document.body.scrollHeight) {
+          basicFoot.classList.remove("sticky");
+        } else if (document.documentElement.scrollTop < window.screen.height) {
+          basicFoot.classList.add("sticky");
+        } else {
+          basicFoot.classList.add("sticky");
         }
-      });
-  
-      var currentPercentage = (window.scrollY / (document.body.scrollHeight - window.screen.height)) * 100;
-  
-      // scroll progress bar
-      document.getElementsByClassName("progress-bar")[0].style.width = currentPercentage + "%";
-  
-      // 좋아요 공유버튼 이벤트 sticky
-      if (document.documentElement.scrollTop == 0 || Math.round(window.scrollY) == document.body.scrollHeight) {
-        basicFoot.classList.remove("sticky");
-      } else if (document.documentElement.scrollTop < window.screen.height) {
-        basicFoot.classList.add("sticky");
-      } else {
-        basicFoot.classList.add("sticky");
-      }
-  
-      // scroll에 따른 main cover image height
-      document.getElementById("cover_full").style.height = window.innerHeight - document.documentElement.scrollTop + "px";
-     
-      var link_opc = (document.getElementById("link_img").offsetHeight -
-      document.getElementsByClassName("wrap_cover")[0].getBoundingClientRect().bottom) /
-      document.getElementById("link_img").offsetHeight;
 
-      // 하단 타이틀
-      document.getElementById("link_title").style.opacity = link_opc;
+        // scroll에 따른 main cover image height
+        coverImg.style.height = window.innerHeight - document.documentElement.scrollTop + "px";
+        coverImg.style.filter = window.innerHeight - document.documentElement.scrollTop + "px";
+
+        break;
+
+      case 1: // 하단이미지 활성
+        linkImg.style.bottom = window.innerHeight - Math.round(body.scrollHeight - window.scrollY - window.innerHeight) + "px";
+        console.log("하단 높이: ", ((body.clientHeight - basicFootBottom) / body.clientHeight) * 100);
+        flag = true;
+        break;
+      case 2: // 하단이미지 비활성
+        console.log("state: ", state);
+        linkImg.style.bottom = "";
+        break;
     }
 
-    
+    // var bodyHeight = body.scrollHeight - basicFoot.scrollHeight - linkImg.scrollHeight;
+    // var scrolledY = window.scrollY - basicFoot.scrollHeight - linkImg.scrollHeight;
+
+    // 화면 하단 비활성화
+    if (flag && linkImg.style.bottom.split("px")[0] < 0) {
+      state = 2;
+    }
+    // 화면의 하단이 5% 보이면 하단 활성화
+    else if (((body.clientHeight - basicFootBottom) / body.clientHeight) * 100 > 5) {
+      state = 1;
+    } else if (linkImg.style.bottom == "") {
+      state = 0;
+    }
   });
 
   // 좋아요 이벤트
