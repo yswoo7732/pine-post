0fb27b8a8c472d92412a52fc6171f5ff45b2bccf
yswoo7732 <wys7732@gmail.com>
2020-11-08 02:21:38 +0900

    05 progressbar 수정 및 소스정리

--- src/0A0005/index.js
+++ src/0A0005/index.js
@@ -35,38 +35,40 @@ window.onload = function () {
     var isUp = true;
     var onSwipe = false;
 
+    var upToWrapBodyScroll = body.scrollHeight - linkCoverFull.scrollHeight + basicFoot.scrollHeight;
     var prevCoverSize = 0;
 
     document.addEventListener("scroll", function (event) {
         event.preventDefault();
         // 스크롤 방향 감지
         if (prevScrollTop > document.documentElement.scrollTop) {
-            console.log("Up");
+            // console.log("Up");
             prevScrollTop = document.documentElement.scrollTop;
 
             isUp = true;
         } else if (prevScrollTop <= document.documentElement.scrollTop) {
-            console.log("Down");
+            // console.log("Down");
             prevScrollTop = document.documentElement.scrollTop;
             isUp = false;
         } else {
-            console.log("no up and down");
+            // console.log("no up and down");
         }
-        console.log("isUp: ", isUp);
+        // console.log("isUp: ", isUp);
 
-        var currentPercentage = Math.round((window.scrollY / (body.scrollHeight - basicFoot.scrollHeight)) * 100) - 3;
 
-        // 하단 이미지 시작점(2px는 progress-bar)
+        // 하단 이미지 시작점과 끝점(2px는 progress-bar)
         var openLinkImgPos = linkCoverFull.offsetTop - window.scrollY + (basicFoot.scrollHeight - 2);
         var closeLinkImgPos = linkCoverFull.offsetTop - window.scrollY;
 
-        var basicFootOpacity = 1 - (window.innerHeight - openLinkImgPos) / window.innerHeight;
-
+        // 하단 이미지 시작시 이미지 scale 변화
         if (window.innerHeight - openLinkImgPos >= 0 && window.innerHeight - openLinkImgPos <= window.innerHeight) {
             var linkImgScale = 1 + (window.innerHeight - openLinkImgPos) / (window.innerHeight * 10);
-            console.log("openLinkImgPos", (window.innerHeight - openLinkImgPos) / (window.innerHeight * 10));
+            // console.log("openLinkImgPos", (window.innerHeight - openLinkImgPos) / (window.innerHeight * 10));
             linkImg.style.transform = `scale(${linkImgScale})`;
         }
+
+        // 하단이미지 시작시, 좋아요/공유버튼 opacity 및 footer change
+        var basicFootOpacity = 1 - (window.innerHeight - openLinkImgPos) / window.innerHeight;
 
         if (window.innerHeight - openLinkImgPos >= 0 && window.innerHeight - openLinkImgPos <= halfBody) {
             basicLinkFoot.style.display = "none";
@@ -97,80 +99,49 @@ window.onload = function () {
             shareImg.style.opacity = 1;
             basicFoot.classList.add("sticky");
         }
-        // 하단 비활성화시키기(활성화된 상태에서 스크롤링 될때만)
-        if (flag == 1 && isUp) {
-            state = 2;
+
+        Array.from(boxShadows).forEach(function (element, index, array) {
+            if (!element.classList.contains("box-shadow-active") && element.offsetTop + height < document.documentElement.scrollTop) {
+                element.classList.add("box-shadow-active");
+            }
+        });
+
+        // scroll progress bar
+        if (window.scrollY >= 0 && upToWrapBodyScroll >= window.scrollY) {
+            if (basicFoot.classList.contains("sticky")) {
+                var currentPercentage = ((window.scrollY + basicFoot.scrollHeight) / upToWrapBodyScroll) * 100;
+            } else {
+                var currentPercentage = (window.scrollY / upToWrapBodyScroll) * 100;
+            }
+            // console.log("currentPercentage ", currentPercentage);
+
+            document.getElementsByClassName("progress-bar")[0].style.width = currentPercentage + "%";
         }
-        // 활성화 시키기(활성화된 상태가 아닐때만)
-        else if (!isUp && flag != 1) {
-            console.log("elseif state 1");
-            state = 1;
+
+        // 좋아요 공유버튼 이벤트 sticky
+        if (document.documentElement.scrollTop == 0 || Math.round(window.scrollY) == document.body.scrollHeight) {
+            basicFoot.classList.remove("sticky");
+        } else if (document.documentElement.scrollTop < window.screen.height) {
+            basicFoot.classList.add("sticky");
         } else {
-            state = 0;
+            basicFoot.classList.add("sticky");
         }
 
-        switch (state) {
-            // 하단 비활성일때 - 메인상태
-            case 0:
-                Array.from(boxShadows).forEach(function (element, index, array) {
-                    // console.log("element.offsetTop", element.offsetTop);
-                    // console.log("document.documentElement.scrollTop", document.documentElement.scrollTop);
-
-                    if (!element.classList.contains("box-shadow-active") && element.offsetTop + height < document.documentElement.scrollTop) {
-                        element.classList.add("box-shadow-active");
-                    }
-                });
-
-                // scroll progress bar
-                document.getElementsByClassName("progress-bar")[0].style.width = currentPercentage + "%";
-
-                // 좋아요 공유버튼 이벤트 sticky
-                if (document.documentElement.scrollTop == 0 || Math.round(window.scrollY) == document.body.scrollHeight) {
-                    basicFoot.classList.remove("sticky");
-                } else if (document.documentElement.scrollTop < window.screen.height) {
-                    basicFoot.classList.add("sticky");
-                } else {
-                    basicFoot.classList.add("sticky");
-                }
-
-                // scroll에 따른 main cover image height
-                if (document.getElementById("body_frame").offsetTop > document.documentElement.scrollTop) {
-                    setTimeout(() => {
-                        prevCoverSize = window.innerHeight - document.documentElement.scrollTop;
-                        coverFull.style.height = prevCoverSize + "px";
-                        // console.log("coverFull.style.height", coverFull.style.height);
-                    }, 3);
-                }
-                break;
-
-            // 하단이미지 활성화시킬때
-            case 1:
-                // linkImg.style.height = window.innerHeight - document.documentElement.scrollTop + "px";
-
-                // setTimeout(() => {
-                //     swipe(event);
-                // }, 1000);
-                flag = 1;
-                break;
-            case 2: // 하단이미지 비활성
-                // console.log("state: ", state);
-                // setTimeout(() => {
-                //     swipeEnd(event);
-                // }, 10);
-                flag = 2;
-                break;
+        // scroll에 따른 main cover image height
+        if (document.getElementById("body_frame").offsetTop >= document.documentElement.scrollTop) {
+            setTimeout(() => {
+                prevCoverSize = window.innerHeight - document.documentElement.scrollTop;
+                coverFull.style.height = prevCoverSize + "px";
+            }, 3);
         }
-        console.log("flag: ", flag);
     });
 
     // 링크 터치 이벤트
     link_title.addEventListener("touchstart", function (event) {
-        console.log("touch!!!");
         this.classList.add("stroke_link-active");
     });
 
     link_title.addEventListener("touchend", function (event) {
-        console.log("touchend!!!");
         this.classList.remove("stroke_link-active");
     });
 
