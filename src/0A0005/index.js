29ccf186537067354e42e7b28ca6e8107c079867
우윤선(Woo Yun Seon) <yunseon.woo@hanwha.com>
2020-11-13 10:43:01 +0900

    test

--- src/0A0005/index.js
+++ src/0A0005/index.js
@@ -164,7 +164,7 @@ window.onload = function () {
                     if (/iPhone/i.test(navigator.userAgent)) {
                         scrollTo(document.documentElement, linkCoverFull.offsetTop, 200, speed);
                     } else {
-                        scrollTo(document.documentElement, linkCoverFull.offsetTop, 60, speed);
+                        scrollTo(document.documentElement, linkCoverFull.offsetTop, 40, speed);
 
                         // TweenMax.to("#link_cover_full", 2, {
                         //     bottom: 200,
