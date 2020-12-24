bb522c8ad5f09731b6d88c87f627658b12481c1f
yunseon.woo1 <yunseon.woo@hanwha.com>
2020-12-24 15:55:00 +0900

    기본형 04 refactoring test

--- src/0A0004/index.js
+++ src/0A0004/index.js
@@ -270,16 +270,20 @@ ScrollTrigger.create({
 });
 
 const ROOT = document.documentElement;
-const MIN = 10;
-const THRESHOLD = innerHeight * (1.2 - 0.225);
+// const MIN = 10;
+// const THRESHOLD = innerHeight * (1.2 - 0.225);
 const update = (e) => {
-    console.log("update");
+    e.preventDefault();
+    if (document.documentElement.scrollTop < 0) return;
     const scroll = Math.floor((scrollY / innerHeight) * 100);
-    if (scrollY > THRESHOLD) {
-        const progress = Math.floor(((scrollY - THRESHOLD) / (document.body.scrollHeight - innerHeight - THRESHOLD)) * 100);
-        ROOT.style.setProperty("--content", progress);
-    }
-    ROOT.style.setProperty("--scroll", Math.max(0, Math.min(scroll, 100 - MIN)));
+    // if (scrollY > THRESHOLD) {
+    //     const progress = Math.floor(((scrollY - THRESHOLD) / (document.body.scrollHeight - innerHeight - THRESHOLD)) * 100);
+    //     ROOT.style.setProperty("--content", progress);
+    // }
+    ROOT.style.setProperty("--scroll", Math.max(0, Math.min(scroll, 100)));
 };
 
 window.addEventListener("scroll", update);
+window.removeEventListener("scroll", function (event) {
+    event.preventDefault();
+});
