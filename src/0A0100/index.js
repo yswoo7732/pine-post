7e5e1e6c540aed45662850f77db9508bf1e15cf0
yswoo7732 <wys7732@gmail.com>
2021-04-09 17:27:34 +0900

    test

--- src/0A0100/index.js
+++ src/0A0100/index.js
@@ -1,11 +1,17 @@
 window.onload = function () {
     const bg = document.getElementById("intro_img");
+    const height = window.innerHeight;
+    const per = height / 300;
+    const imgHeight = height / 3 * 2;
 
-    window.addEventListener('scroll', function() {
-        if (pageYOffset <= 564) {
-            bg.style.backgroundSize = 300 - +window.pageYOffset/2.82+'%';
+    console.log(imgHeight);
 
+    window.addEventListener('scroll', function () {
+
+        if (pageYOffset <= imgHeight) {
+            console.log(window.pageYOffset);
+
+            bg.style.backgroundSize = 300 - +window.pageYOffset / per + '%';
         }
-        bg.style.backgroundPositionY = 0;
     });
 };
