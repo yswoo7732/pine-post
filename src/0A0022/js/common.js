49f871df97cb47fbe99ae0362a4309651947e04f
20200057A <yunseon.woo@hanwha.com>
2022-12-01 10:25:25 +0900

    refact: 콘텐츠 페이지 좋아요 아이콘 제거(PN1-1560) refact: 콘텐츠 타이틀바 개선(PN1-2378)

--- src/0A0022/js/common.js
+++ src/0A0022/js/common.js
@@ -1,179 +1,156 @@
 var console = window.console || { log: function () {} };
 var ds_config;
 var getMetaValue = (function () {
-    var metas = {};
-    var metaGetter = function (metaName) {
-        wasDOMQueried = true;
-        if (metas[metaName]) {
-            wasDOMQueried = false;
-        } else {
-            Array.prototype.some.call(
-                document.getElementsByTagName("meta"),
-                function (el) {
-                    if (el.name === metaName) {
-                        metas[metaName] = el.content;
-                        return true;
-                    }
-                    if (el.getAttribute("property") === metaName) {
-                        metas[metaName] = el.content;
-                        return true;
-                    } else {
-                        metas[metaName] = "";
-                    }
-                }
-            );
+  var metas = {};
+  var metaGetter = function (metaName) {
+    wasDOMQueried = true;
+    if (metas[metaName]) {
+      wasDOMQueried = false;
+    } else {
+      Array.prototype.some.call(
+        document.getElementsByTagName("meta"),
+        function (el) {
+          if (el.name === metaName) {
+            metas[metaName] = el.content;
+            return true;
+          }
+          if (el.getAttribute("property") === metaName) {
+            metas[metaName] = el.content;
+            return true;
+          } else {
+            metas[metaName] = "";
+          }
         }
-        // console.info("Q:wasDOMQueried? A:" + wasDOMQueried);
-        // console.info(metas);
-        // console.log(metas[metaName]);
-        return metas[metaName];
-    };
-    return metaGetter;
+      );
+    }
+    // console.info("Q:wasDOMQueried? A:" + wasDOMQueried);
+    // console.info(metas);
+    // console.log(metas[metaName]);
+    return metas[metaName];
+  };
+  return metaGetter;
 })();
 
-// if (navigator.platform) {
-//     ds_config = {
-//         isMobile: isMobile(),
-//         appkey: "9990a117fdd5177a8ba274d0057728cd",
-//         kakaotalk: {
-//             id: "kakao-link",
-//             width: "610",
-//             height: "319",
-//             text: getMetaValue("og:description"),
-//         },
-//         title: getMetaValue("og:title"),
-//         image: getMetaValue("og:image"),
-//         url: getMetaValue("og:url"),
-//     };
-//     // console.log(getMetaContent("og:title"));
-//     Kakao.init(ds_config.appkey);
+function goBack() {
+  if (/Android/i.test(navigator.userAgent)) {
+    window.AosConnector.requestExitMenu();
+  }
 
-//     var console_style = [
-//         "padding : 18px 32px 18px 24px",
-//         "margin : 10px 0",
-//         "background : #00526d",
-//         "font-size : 20px",
-//         "font-weight : bold",
-//         "text-align : center",
-//         "color : #ffffff",
-//     ].join(";");
-// }
+  if (/iPhone|iPad|iPod/i.test(navigator.userAgent)) {
+    webkit.messageHandlers.requestExitMenu.postMessage("");
+  }
+}
 
 function isMobile() {
-    var rtn = false;
-    // if (window.innerWidth <= 640) {
-    //     rtn = true;
-    // }
-    if (
-        navigator.userAgent.match(
-            /Android|Mobile|iP(hone|od|ad)|BlackBerry|IEMobile|Kindle|NetFront|Silk-Accelerated|(hpw|web)OS|Fennec|Minimo|Opera M(obi|ini)|Blazer|Dolfin|Dolphin|Skyfire|Zune/
-        )
-    ) {
-        rtn = true;
-    }
-    return rtn;
+  var rtn = false;
+  // if (window.innerWidth <= 640) {
+  //     rtn = true;
+  // }
+  if (
+    navigator.userAgent.match(
+      /Android|Mobile|iP(hone|od|ad)|BlackBerry|IEMobile|Kindle|NetFront|Silk-Accelerated|(hpw|web)OS|Fennec|Minimo|Opera M(obi|ini)|Blazer|Dolfin|Dolphin|Skyfire|Zune/
+    )
+  ) {
+    rtn = true;
+  }
+  return rtn;
 }
 
 function shuffle(a) {
-    var j, x, i;
-    for (i = a.length; i; i -= 1) {
-        j = Math.floor(Math.random() * i);
-        x = a[i - 1];
-        a[i - 1] = a[j];
-        a[j] = x;
-    }
+  var j, x, i;
+  for (i = a.length; i; i -= 1) {
+    j = Math.floor(Math.random() * i);
+    x = a[i - 1];
+    a[i - 1] = a[j];
+    a[j] = x;
+  }
 }
 
 function getParameter(param) {
-    var returnValue;
-    var url = location.href;
-    var parameters = url.slice(url.indexOf("?") + 1, url.length).split("&");
+  var returnValue;
+  var url = location.href;
+  var parameters = url.slice(url.indexOf("?") + 1, url.length).split("&");
 
-    for (var i = 0; i < parameters.length; i++) {
-        var varName = parameters[i].split("=")[0];
-        if (varName.toUpperCase() == param.toUpperCase()) {
-            returnValue = parameters[i].split("=")[1];
-            return decodeURIComponent(returnValue);
-        }
+  for (var i = 0; i < parameters.length; i++) {
+    var varName = parameters[i].split("=")[0];
+    if (varName.toUpperCase() == param.toUpperCase()) {
+      returnValue = parameters[i].split("=")[1];
+      return decodeURIComponent(returnValue);
     }
+  }
 }
 
-
 function sendSns(sns, url, txt) {
-    var o;
-    var _url = encodeURIComponent(url);
-    var _txt = encodeURIComponent(txt);
-    //    var _br = encodeURIComponent('\r\n');
-    // console.log(" ____ " + ds_config.url)
+  var o;
+  var _url = encodeURIComponent(url);
+  var _txt = encodeURIComponent(txt);
+  //    var _br = encodeURIComponent('\r\n');
+  // console.log(" ____ " + ds_config.url)
 
-    switch (sns) {
-        case "facebook":
-            o = {
-                method: "popup",
-                url: "https://www.facebook.com/sharer/sharer.php?u=" + _url,
-            };
-            break;
+  switch (sns) {
+    case "facebook":
+      o = {
+        method: "popup",
+        url: "https://www.facebook.com/sharer/sharer.php?u=" + _url,
+      };
+      break;
 
-        case "twitter":
-            o = {
-                method: "popup",
-                url:
-                    "https://twitter.com/intent/tweet?text=" +
-                    _txt +
-                    "&url=" +
-                    _url,
-            };
-            break;
-        case "kakaotalk":
-            // console.log("ds_config.title : " + ds_config.title + " : ds_config.kakaotalk.text : " + ds_config.kakaotalk.text + " _ " + ds_config.url)
-            Kakao.Link.sendDefault({
-                objectType: "feed",
-                content: {
-                    title: ds_config.title,
-                    description: ds_config.kakaotalk.text,
-                    imageUrl: getMetaValue("og:image:kakao")
-                        ? getMetaValue("og:image:kakao")
-                        : ds_config.image,
-                    link: {
-                        mobileWebUrl: ds_config.url,
-                        webUrl: ds_config.url,
-                    },
-                },
-                buttons: [
-                    {
-                        title: "",
-                        link: {
-                            mobileWebUrl: "",
-                            webUrl: "",
-                        },
-                    },
-                ],
-                fail: function () {
-                    alert("지원하지 않는 플랫폼입니다.");
-                },
-            });
-            break;
+    case "twitter":
+      o = {
+        method: "popup",
+        url: "https://twitter.com/intent/tweet?text=" + _txt + "&url=" + _url,
+      };
+      break;
+    case "kakaotalk":
+      // console.log("ds_config.title : " + ds_config.title + " : ds_config.kakaotalk.text : " + ds_config.kakaotalk.text + " _ " + ds_config.url)
+      Kakao.Link.sendDefault({
+        objectType: "feed",
+        content: {
+          title: ds_config.title,
+          description: ds_config.kakaotalk.text,
+          imageUrl: getMetaValue("og:image:kakao")
+            ? getMetaValue("og:image:kakao")
+            : ds_config.image,
+          link: {
+            mobileWebUrl: ds_config.url,
+            webUrl: ds_config.url,
+          },
+        },
+        buttons: [
+          {
+            title: "",
+            link: {
+              mobileWebUrl: "",
+              webUrl: "",
+            },
+          },
+        ],
+        fail: function () {
+          alert("지원하지 않는 플랫폼입니다.");
+        },
+      });
+      break;
 
-        default:
-            alert("");
-            return false;
-    }
+    default:
+      alert("");
+      return false;
+  }
 
-    switch (o.method) {
-        case "popup":
-            window.open(
-                o.url,
-                o.sns + "",
-                "scrollbars=yes,toolbar=yes,resizable=yes,width=600,height=400"
-            );
-            break;
-    }
+  switch (o.method) {
+    case "popup":
+      window.open(
+        o.url,
+        o.sns + "",
+        "scrollbars=yes,toolbar=yes,resizable=yes,width=600,height=400"
+      );
+      break;
+  }
 }
 
 function urlCopy() {
-    var urlbox = document.getElementById("textUrl");
-    urlbox.value = window.location.href;
-    urlbox.select();
-    document.execCommand("Copy");
-    alert("URL이 복사 되었습니다.");
+  var urlbox = document.getElementById("textUrl");
+  urlbox.value = window.location.href;
+  urlbox.select();
+  document.execCommand("Copy");
+  alert("URL이 복사 되었습니다.");
 }
--- src/common/card.js
+++ src/common/card.js
@@ -1,69 +1,90 @@
-function getLike() {
-    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent)) {
-        if (/PINE/i.test(navigator.userAgent)) {
-            // document.querySelector(".footer").style.paddingBottom = "calc(constant(safe-area-inset-bottom) + 34px)";
-            // document.querySelector(".footer").style.paddingBottom = "calc(env(safe-area-inset-bottom) + 34px)";
-
-            // 좋아요 클릭 유무 체크위해 호출
-            if (/Android/i.test(navigator.userAgent)) {
-                window.AosConnector.isLike();
-            }
-
-            if (/iPhone|iPad|iPod/i.test(navigator.userAgent)) {
-                webkit.messageHandlers.isLike.postMessage();
-            }
-        }
-    } else {
-        console.log("not mobile");
+document.addEventListener("DOMContentLoaded", function () {
+  if (navigator.userAgent.match(/PINE/)) {
+    if (/Android/i.test(navigator.userAgent)) {
+      window.AosConnector.hideTitleBar();
     }
+    if (/iPhone|iPad|iPod/i.test(navigator.userAgent)) {
+      webkit.messageHandlers.hideTitleBar.postMessage(""); //ios
+    }
+  }
+});
+
+function getLike() {
+  if (/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent)) {
+    if (/PINE/i.test(navigator.userAgent)) {
+      // document.querySelector(".footer").style.paddingBottom = "calc(constant(safe-area-inset-bottom) + 34px)";
+      // document.querySelector(".footer").style.paddingBottom = "calc(env(safe-area-inset-bottom) + 34px)";
+
+      // 좋아요 클릭 유무 체크위해 호출
+      if (/Android/i.test(navigator.userAgent)) {
+        window.AosConnector.isLike();
+      }
+
+      if (/iPhone|iPad|iPod/i.test(navigator.userAgent)) {
+        webkit.messageHandlers.isLike.postMessage();
+      }
+    }
+  } else {
+    console.log("not mobile");
+  }
 }
 
 // 공유하기 클릭
 function sharedContents() {
-    console.log("shared");
-    let contentsUrl = window.location.href;
+  console.log("shared");
+  let contentsUrl = window.location.href;
 
-    if (/PINE/i.test(navigator.userAgent)) {
-        if (/Android/i.test(navigator.userAgent)) {
-            window.AosConnector.shareContents(`${contentsUrl}`); // Aos
-        }
-        if (/iPhone|iPad|iPod/i.test(navigator.userAgent)) {
-            webkit.messageHandlers.shareContents.postMessage(`${contentsUrl}`); //ios
-        }
+  if (/PINE/i.test(navigator.userAgent)) {
+    if (/Android/i.test(navigator.userAgent)) {
+      window.AosConnector.shareContents(`${contentsUrl}`); // Aos
     }
+    if (/iPhone|iPad|iPod/i.test(navigator.userAgent)) {
+      webkit.messageHandlers.shareContents.postMessage(`${contentsUrl}`); //ios
+    }
+  }
 }
 
 // 컨텐츠 로딩시, 이전 좋아요 클릭 체크
 function isLike(yn) {
-    console.log("isLike");
+  console.log("isLike");
 
-    let imgHeart = document.querySelector("#imgHeart");
-    imgHeart.classList.toggle("heart_active");
+  let imgHeart = document.querySelector("#imgHeart");
+  imgHeart.classList.toggle("heart_active");
 
-    if (yn == "N") {
-        imgHeart.classList.remove("heart_active");
-    } else {
-        imgHeart.classList.add("heart_active");
-    }
+  if (yn == "N") {
+    imgHeart.classList.remove("heart_active");
+  } else {
+    imgHeart.classList.add("heart_active");
+  }
 }
 
 // 좋아요 클릭시
 function clickLike() {
-    let imgHeart = document.querySelector("#imgHeart");
-    let likeYN = "N";
+  let imgHeart = document.querySelector("#imgHeart");
+  let likeYN = "N";
 
-    imgHeart.classList.toggle("heart_active");
+  imgHeart.classList.toggle("heart_active");
 
-    if (imgHeart.classList.contains("heart_active")) {
-        likeYN = "Y";
-    } else {
-        likeYN = "N";
-    }
+  if (imgHeart.classList.contains("heart_active")) {
+    likeYN = "Y";
+  } else {
+    likeYN = "N";
+  }
 
-    if (/Android/i.test(navigator.userAgent)) {
-        window.AosConnector.clickedLike(`${likeYN}`);
-    }
-    if (/iPhone|iPad|iPod/i.test(navigator.userAgent)) {
-        webkit.messageHandlers.clickedLike.postMessage(`${likeYN}`);
-    }
+  if (/Android/i.test(navigator.userAgent)) {
+    window.AosConnector.clickedLike(`${likeYN}`);
+  }
+  if (/iPhone|iPad|iPod/i.test(navigator.userAgent)) {
+    webkit.messageHandlers.clickedLike.postMessage(`${likeYN}`);
+  }
+}
+
+function goBack() {
+  if (/Android/i.test(navigator.userAgent)) {
+    window.AosConnector.requestExitMenu();
+  }
+
+  if (/iPhone|iPad|iPod/i.test(navigator.userAgent)) {
+    webkit.messageHandlers.requestExitMenu.postMessage("");
+  }
 }
