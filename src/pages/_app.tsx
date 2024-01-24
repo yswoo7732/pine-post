a797b071e382ad5ae37c937fb49a00f687fa47b5
20200057A <yunseon.woo@hanwha.com>
2024-01-24 17:50:09 +0900

    chore: test

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,33 +1,28 @@
-import { QueryClientProvider, Hydrate } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import { queryClient } from '@/lib/react-query';
 import { AppPropsWithLayout } from '@/types';
-import { RootLayout } from '@/layouts';
-import NextNProgress from 'nextjs-progressbar';
-import '@/styles/globals.css';
 
 function App({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout || (page => page);
   const { title } = pageProps;
 
-  return (
-    <QueryClientProvider client={queryClient}>
-      <Hydrate state={pageProps.dehydratedState}>
-        <NextNProgress
-          color={'#00C851'}
-          startPosition={0.3}
-          stopDelayMs={200}
-          height={1}
-          showOnShallow={true}
-          options={{ showSpinner: false }}
-        />
-          {getLayout(<Component {...pageProps} />)}
-        {/* <RootLayout title={title}> */}
-        {/* </RootLayout> */}
-      </Hydrate>
-      <ReactQueryDevtools />
-    </QueryClientProvider>
-  );
+  return <Component {...pageProps} />;
+  // return (
+  //   <QueryClientProvider client={queryClient}>
+  //     <Hydrate state={pageProps.dehydratedState}>
+  //       <NextNProgress
+  //         color={'#00C851'}
+  //         startPosition={0.3}
+  //         stopDelayMs={200}
+  //         height={1}
+  //         showOnShallow={true}
+  //         options={{ showSpinner: false }}
+  //       />
+  //         {getLayout(<Component {...pageProps} />)}
+  //       {/* <RootLayout title={title}> */}
+  //       {/* </RootLayout> */}
+  //     </Hydrate>
+  //     <ReactQueryDevtools />
+  //   </QueryClientProvider>
+  // );
 }
 
 export default App;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,79 +8,81 @@ import PostListRepresent from '@/components/PostListRepresent';
 import { GetServerSideProps, GetStaticProps, NextPage } from 'next/types';
 import { queryKey } from '@/constants/queryKey';
 
-export const getStaticProps: GetStaticProps = async () => {
-  try {
-    // 데이터를 가져오는 비동기 함수 호출
-    const data = await getCategoryDatabases();
+// export const getStaticProps: GetStaticProps = async () => {
+//   try {
+//     // 데이터를 가져오는 비동기 함수 호출
+//     const data = await getCategoryDatabases();
 
-    if (!data) {
-      // 데이터가 없으면 에러 발생
-      throw new Error('Failed to fetch data');
-    }
+//     if (!data) {
+//       // 데이터가 없으면 에러 발생
+//       throw new Error('Failed to fetch data');
+//     }
 
-    // 각 페이지의 url을 사용하여 추가 데이터를 가져오기
-    const additionalDataPromises = data.results.map(async category => {
-      const categoryId = category.id;
-      const additionalData = await fetchAdditionalData(categoryId);
-      return { categoryId, additionalData, category };
-    });
+//     // 각 페이지의 url을 사용하여 추가 데이터를 가져오기
+//     const additionalDataPromises = data.results.map(async category => {
+//       const categoryId = category.id;
+//       const additionalData = await fetchAdditionalData(categoryId);
+//       return { categoryId, additionalData, category };
+//     });
 
-    // Promise.all을 사용하여 모든 페이지의 추가 데이터를 기다림
-    const pagesWithData = await Promise.all(additionalDataPromises);
+//     // Promise.all을 사용하여 모든 페이지의 추가 데이터를 기다림
+//     const pagesWithData = await Promise.all(additionalDataPromises);
 
-    return {
-      props: {
-        data: {
-          ...data,
-          results: pagesWithData,
-        },
-      },
-      revalidate: CONFIG.revalidateTime,
-    };
-  } catch (error) {
-    // 데이터를 가져오는 중에 오류가 발생하면 에러를 콘솔에 출력
-    console.error('Error fetching data:', error.message);
+//     return {
+//       props: {
+//         data: {
+//           ...data,
+//           results: pagesWithData,
+//         },
+//       },
+//       revalidate: CONFIG.revalidateTime,
+//     };
+//   } catch (error) {
+//     // 데이터를 가져오는 중에 오류가 발생하면 에러를 콘솔에 출력
+//     console.error('Error fetching data:', error.message);
 
-    // 빈 데이터로 반환 또는 에러 처리에 따라 적절히 조치
-    return {
-      props: {
-        data: {},
-      },
-      revalidate: CONFIG.revalidateTime,
-    };
-  }
-};
+//     // 빈 데이터로 반환 또는 에러 처리에 따라 적절히 조치
+//     return {
+//       props: {
+//         data: {},
+//       },
+//       revalidate: CONFIG.revalidateTime,
+//     };
+//   }
+// };
 
 // 각 페이지의 url을 받아와서 추가 데이터를 가져오는 비동기 함수
-async function fetchAdditionalData(categoryId: string) {
-  const filter = {
-    property: 'category',
-    relation: {
-      contains: categoryId,
-    },
-  };
-  const additionalData = await getFilteredDatabases(filter);
+// async function fetchAdditionalData(categoryId: string) {
+//   const filter = {
+//     property: 'category',
+//     relation: {
+//       contains: categoryId,
+//     },
+//   };
+//   const additionalData = await getFilteredDatabases(filter);
 
-  if (additionalData?.results) {
-    return additionalData.results;
-  }
-}
+//   if (additionalData?.results) {
+//     return additionalData.results;
+//   }
+// }
 
-const Home: NextPage<IndexPageProps> = data => {
-  const meta = {
-    title: CONFIG.blog.title,
-    description: CONFIG.blog.description,
-    type: 'website',
-    url: CONFIG.link,
-  };
+const Home = () => {
+  return (<></>)
+}// const Home: NextPage<IndexPageProps> = data => {
+//   const meta = {
+//     title: CONFIG.blog.title,
+//     description: CONFIG.blog.description,
+//     type: 'website',
+//     url: CONFIG.link,
+//   };
 
-  console.log(data);
-  return (
-    <>
-      <MetaConfig {...meta} />
-      <PostListRepresent data={data} />
-    </>
-  );
-};
+//   console.log(data);
+//   return (
+//     <>
+//       <MetaConfig {...meta} />
+//       <PostListRepresent data={data} />
+//     </>
+//   );
+// };
 
 export default Home;
